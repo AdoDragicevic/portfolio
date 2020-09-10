68f3a183fd083fa068300f23fd4c2957339da69e
Ado Dragicevic <adodragicevic@gmail.com>
2020-09-10 19:40:06 +0000

    fix width on mobile

--- public/js/js.js
+++ public/js/js.js
@@ -1,8 +1,12 @@
 // show/hide back to top arrow when page is scrolled
-$(document).scroll(function () {	
+document.body.addEventListener('scroll', function(){
 	if($(this).scrollTop() > 500){
 		$("#arrow").removeClass("d-none");
 	}else{
 		$("#arrow").addClass("d-none");
 	}	
+});
+
+$("#arrow").click(function(){
+	document.body.scrollTop();
 });
