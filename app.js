8fddf4cab7fcc91032abe241a7c380d2bb30b356
Ado Dragicevic <adodragicevic@gmail.com>
2020-09-20 10:43:09 +0000

    add start script adn app.listen for heroku

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// PORT SETUP
+const port = process.env.PORT || 3000;
+
 // ROUTES
 app.get("/", function(req, res){
 	res.render("index");
 });
 
-app.listen(3000, function(){
-	console.log("SERVER RUNNING");
+app.listen(port, function () {
+  console.log("SERVER RUNNING...");
 });
